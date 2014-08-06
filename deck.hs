94b0b441b0bd57f2b5e549b9fa7ec35c2e0d54ec
Ryan S. Brown <sb@ryansb.com>
2014-08-06 14:06:17 -0400

    first working cipher step function

--- deck.hs
+++ deck.hs
@@ -4,7 +4,9 @@ jA = 53
 -- 54 is Joker B
 jB = 54
 
-pack = [1..54]
+pack      = [1..54]
+testPack  = [1..20] ++ [jA] ++ [21..40] ++ [jB] ++ [41..52]
+testPack' = [1..20] ++ [jB] ++ [21..40] ++ [jA] ++ [41..52]
 
 -- TODO: Actually take input for key
 rawkey = toStream (take 10 (repeat 'A'))
@@ -20,11 +22,38 @@ cutDeck deck =
     in tail (fst v) ++ [head deck] ++ snd v
 
 
-moveJokerA deck
-    | (head deck) == jA = (init (tail deck) ++ [(head deck), (last deck)]) -- A Joker is top
-    | otherwise       = let (ys,zs) = splitAt ((length (takeWhile (/=jA) deck)) - 1) (filter (/=jA) deck) in ys ++ [jA] ++ zs
+moveJoker j deck
+    | last deck == j = j : (init deck) -- Joker is bottom
+    | otherwise      =
+        let (ys,zs) = splitAt (length (takeWhile (/=j) deck) + 1) (filter (/=j) deck)
+        in ys ++ (j : zs)
 
-moveJokerB deck
-    | head deck == jB = let (ys, zs) = splitAt ((length deck) - 2) deck in (tail ys) ++ [ head deck ] ++ zs
-    | deck !! 1 == jB = let (ys, zs) = splitAt ((length deck) - 1) deck in (filter (/=jB) ys) ++ [ jB ] ++ zs
-    | otherwise       = let (ys,zs) = splitAt ((length (takeWhile (/=jB) deck)) - 1) (filter (/=jB) deck) in ys ++ [jB] ++ zs
+moveJokerA deck = moveJoker jA deck
+moveJokerB deck = moveJoker jB (moveJoker jB deck)
+
+moveJokers deck = moveJokerB (moveJokerA deck)
+
+tripleCut deck =
+  let front    = takeWhile (<53) deck
+      middle   = takeWhile (<53) (drop ((length front) + 1) deck)
+      back     = tail (dropWhile (<53) (tail (dropWhile (<53) deck)))
+      jOrder   = filter (>=53) deck
+  in back ++ [head jOrder] ++ middle ++ [last jOrder] ++ front
+
+countCut deck
+    | last deck >= 53 = deck
+    | otherwise       =
+        let offset  = fromIntegral (last deck)
+            front   = take offset deck
+            middle  = drop offset deck
+        in (init middle) ++ front ++ [last deck]
+
+cardN card deck = fromIntegral (deck !! card)
+
+streamCard deck = fromIntegral (deck !! (cardN 0 deck))
+
+streamChar' deck = streamCard (deckStep deck)
+
+deckStep deck = countCut (tripleCut (moveJokers deck))
+
+cipherStep deck = (streamChar' deck, deckStep deck)
